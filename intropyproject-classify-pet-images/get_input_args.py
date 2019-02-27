ecfacba5158564cf281578f70f97cc13b768f5c1
David Jarrin <dmjarrin@gmail.com>
2019-02-27 05:44:12 -0500

    adding to comments

--- intropyproject-classify-pet-images/get_input_args.py
+++ intropyproject-classify-pet-images/get_input_args.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
 #
-# PROGRAMMER:
-# DATE CREATED:
+# PROGRAMMER: David Jarrin
+# DATE CREATED: 2/27/19
 # REVISED DATE:
 # PURPOSE: Create a function that retrieves the following 3 command line inputs
 #          from the user using the Argparse Python module. If the user fails to
